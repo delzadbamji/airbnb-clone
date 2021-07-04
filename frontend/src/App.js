58fd3d67989523f4bb947dd8d90988c9f2c6981b
Delzad <delzadbamji@gmail.com>
2021-07-03 21:03:46 -0400

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import SignUp from "./views/SignUp/index";
 import Register from "./views/Register/index";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+
 function App() {
-  const [isLoggedIn,setisLoggedIn] = useState(false);
+  const [isLoggedIn, setisLoggedIn] = useState(false);
   return (
     <div className="App">
-       <NavBar isLoggedIn={isLoggedIn} /> 
-      // put the homepage here. the NavBar will toggle content based on the login status
+      <NavBar isLoggedIn={isLoggedIn} />
+      {/* put the homepage here. the NavBar will toggle content based on the login status*/}
     </div>
   );
 }
