dd6aabaa0df13f92b9fb5576c32df9150dc0b3dd
Delzad <delzadbamji@gmail.com>
2021-07-12 22:44:05 -0400

    error404 view styled

--- frontend/src/views/Error404/index.js
+++ frontend/src/views/Error404/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./styles.css";
+import Exclamation404 from "../../assets/exclamation-404.svg";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
-  return <div> Oops </div>
+  return (
+    // <div className="container">
+    <>
+      <img src={Exclamation404} className="image404" alt="404" />
+      <div className="notFoundMessage">Oops! Page not found</div>
+
+      <div className="notFoundMessage" style={{ top: "73%" }}>
+        <Link to="/" style={{ color: "black" }}>
+          Navigate back to home
+        </Link>
+      </div>
+    </>
+  );
 };
 export default Error404;
